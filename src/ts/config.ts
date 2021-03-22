232993efed3f4bfeefef447a0cb7ea6e3d9fcbfa
Tobias de Bruijn <github@thedutchmc.nl>
2021-03-22 22:49:46 +0100

    Update endpoint

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,2 +1,2 @@
-export const REGISTER_ENDPOINT = "https://api.thedutchmc.nl/honeywell_google_sync/auth/register"
-export const LOGIN_ENDPOINT = "https://api.thedutchmc.nl/honeywell_google_sync/auth/login"
+export const REGISTER_ENDPOINT = "https://api.thedutchmc.nl/smarthome/auth/register"
+export const LOGIN_ENDPOINT = "https://api.thedutchmc.nl/smarthome/auth/login"
